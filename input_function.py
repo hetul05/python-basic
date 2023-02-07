0c275feb8897faf340debc9f38c16381f0f908a5
Hetul shah <75409654+hetul05@users.noreply.github.com>
2023-02-07 22:37:25 +0530

    input function

---
+++ input_function.py
@@ -0,0 +1,6 @@
+a=input("enter your name:")
+print(a)# the type of output will be always string
+
+a=input("enter your number:")
+a=int(a)# by this method(typecasting) it will convert str to int
+print(type(a))
