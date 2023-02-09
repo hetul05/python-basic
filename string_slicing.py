5571059c4c935f8d258db83555b85c657abd6a60
Hetul shah <75409654+hetul05@users.noreply.github.com>
2023-02-09 11:41:49 +0530

    string slicing

---
+++ string_slicing.py
@@ -0,0 +1,8 @@
+name="hetul"
+print(name[0:1])# string slicing
+print(name[0:2])
+print(name[0:3])
+print(name[0:4])
+print(name[0:5])# it will print full string
+print(name[0:6])
+print(name[0:10000])
