090056546845b557407ea08e445750cbc675a0b7
Hetul shah <75409654+hetul05@users.noreply.github.com>
2023-02-11 23:58:29 +0530

    string_functions

---
+++ string_functions.py
@@ -0,0 +1,6 @@
+story="hetul shah is trying to learn python"
+print(len(story))#len function is used to count total number of characters
+print(story.endswith("on"))#it will check the ending part of string and return accordingly
+print(story.count("h"))#it wil check the occurance of h in string
+print(story.capitalize())#it will capitalize the first word of string
+print(story.find("is"))
