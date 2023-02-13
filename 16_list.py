3e18558747f309d2936cf3b22bc9f1856b0728e5
Hetul shah <75409654+hetul05@users.noreply.github.com>
2023-02-13 16:54:52 +0530

    list

---
+++ 16_list.py
@@ -0,0 +1,7 @@
+#list is a container which stores the value of any data types like
+#string,float,int etc. 
+list=["hetul",56,24.3]
+print(list)
+
+print(list[0])#here we can access through indexing
+
