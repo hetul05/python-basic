2fa4309c35789b7d77d732e1f80fd88c67c19323
Hetul shah <75409654+hetul05@users.noreply.github.com>
2023-02-13 22:39:05 +0530

    list

---
+++ 18_list_methods.py
@@ -0,0 +1,20 @@
+#python methods
+#sort ---- it will sort the list in ascendind order
+name=[12,25,14,86,2,9,]
+name.sort()# it will updated the original list we dont need to store in another fn
+print(name)
+#reverse-#it will reverse the list NOTE----it will not be in descending order ,it will reverse the list
+name.reverse()
+print(name)
+#append --- add at the end of the list
+name.append(34)
+print(name)
+#insert --- insert at specified index
+name.insert(0,1)
+print(name)
+#pop---it wil remove the item from index
+name.pop(0)
+print(name)
+#remove------------it remove the number of your list
+name.remove(14)
+print(name)
