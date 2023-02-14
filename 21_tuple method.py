3b1e07ee9f799b7aa6e98362e538ab187b18bc32
Hetul shah <75409654+hetul05@users.noreply.github.com>
2023-02-14 16:43:09 +0530

    tuple_methods

---
+++ 21_tuple method.py
@@ -0,0 +1,15 @@
+# a=(1,2,4,5)
+# a.count(2)
+# print(a)
+#here we dont do like above method because in tuple 
+# we are not changing the value inside the tuple we are 
+# only accessing the tuple
+
+
+a=(2,5,4,5,7)
+b=a.index(5)
+print(b)# it will return the index value of 5
+
+a=(2,5,4,5,7)
+b=a.count(5)
+print(b)# it will count the occurence of 5
