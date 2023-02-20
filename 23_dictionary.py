f94008a68c88b25a7bb9e301e6e946c1267b74f4
Hetul shah <75409654+hetul05@users.noreply.github.com>
2023-02-20 14:10:31 +0530

    nested_dictionary

--- 23_dictionary.py
+++ 23_dictionary.py
@@ -1,8 +1,10 @@
 mydictionary={
     "fast":"a quick access",
     "hetul":"a boy",
-    "marks":54
+    "marks":54,
+    "another dic":{"harry":"child"}#AS
 }
 print(mydictionary["fast"])
 print(mydictionary["hetul"])
 print(mydictionary["marks"])
+print(mydictionary["another dic"]["harry"])# print nested dictionary 
---
+++ nested_dictionary.py
@@ -0,0 +1,7 @@
+homies={
+    "homie1":"ghi",
+    "homie2":"cdf",
+    "homie3":"xyz",
+    "homie4":{"ehomie":"aa","ehomies":"fg"} # here we have created a nested dictionary
+}
+print(homies["homie4"]["ehomie"])
