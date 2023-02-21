7fd697e1e365fdeed0f6c95d2458042255f954e6
Hetul shah <75409654+hetul05@users.noreply.github.com>
2023-02-21 10:28:35 +0530

    sets

---
+++ 25_sets.py
@@ -0,0 +1,8 @@
+a={1,5,5,9}
+print(a)# print only 1,5,9 because set is collection of non repetitve elements
+print(type(a))# returns the type 
+
+b=set()# empty set 
+print(type(b))
+b.add(4)# adding 4 to empty set
+print(b)
