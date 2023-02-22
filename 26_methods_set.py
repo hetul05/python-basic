ba00466c409774568fc79579a8b7613feb24518b
Hetul shah <75409654+hetul05@users.noreply.github.com>
2023-02-22 09:06:32 +0530

    method of sets

---
+++ 26_methods_set.py
@@ -0,0 +1,22 @@
+# a=set()# create an empty set for
+
+# a.add(5)
+# a.add(9)
+# a.add(5)# it will not add 5 second time because set is collection of non repitive elemets
+# print(type(a))
+# print(a)
+
+# # a.add([45,4,9])
+# # print(a)# will throw an error because list can be changed thus error
+# a.add((45,4,9))# we can add tuple in set becoz it is non changeable
+# print(a)
+
+
+s={1,5,7,8}
+# b=s.remove(1)# removes the 1 element from the set
+# c=len(s)# return the length of the set
+# s.pop()# it will remove the random element
+# s.clear()# clear the set
+# s.union({5,8,2})# it will add the set into existing set 
+
+print(s)
