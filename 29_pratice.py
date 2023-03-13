a346ca4b2d56f95f6d9663a65a8f810f14265861
Hetul shah <75409654+hetul05@users.noreply.github.com>
2023-03-13 12:43:50 +0530

    pratice

---
+++ 29_pratice.py
@@ -0,0 +1,12 @@
+# question enter your  fav song and  your frd name and shown them in dic 
+a=input("hetul enter your fav song:\n")
+b=input("parth enter your fav song:\n")
+c=input("faizal enter your fav song:\n")
+d=input("yatharth enter your fav song:\n")
+dic={
+    "hetul":"a",
+    "parth":"b",
+    "faizal":"c",
+    "yatharth":"d"
+}
+print(dic)
