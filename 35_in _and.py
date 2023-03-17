ea8ae3a536bca54c6f14cc29374cb9db2e9c8005
Hetul shah <75409654+hetul05@users.noreply.github.com>
2023-03-17 23:02:59 +0530

    in_and in if elif

---
+++ 35_in _and.py
@@ -0,0 +1,9 @@
+a=None
+if(a is None):# the main function of (is) same as (=)
+    print("true")
+else:
+    print("false")
+    
+# the  in function checks that 45 in list
+a=[45,54,32]
+print(45 in a)       
