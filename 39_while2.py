cbd646a1adfcb622083f107caea6c4216607fe3d
Hetul shah <75409654+hetul05@users.noreply.github.com>
2023-03-21 09:15:24 +0530

    updated

--- 39_while2.py
+++ 39_while2.py
@@ -2,3 +2,4 @@ i=0
 while i<=50: # this will check if the condition is true or not
     print(i) # this will print
     i=i+1 #this will increment the value of i 
+    # here we cannot use (++) for increment because python doesnt allow that
