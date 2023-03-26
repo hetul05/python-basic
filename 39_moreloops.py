4624be5664f7d582c79bdb614849074d715ae4f6
Hetul shah <75409654+hetul05@users.noreply.github.com>
2023-03-26 21:27:13 +0530

    for loop

---
+++ 39_moreloops.py
@@ -0,0 +1,11 @@
+# to print items in list 
+len=[25,65,87]
+for i in len:
+    print(i)
+
+# to print a items in list using another method
+l=[48,58,69]
+for items in l:
+    print(items)
+    
+    
