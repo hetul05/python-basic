5f59c2ffae01666c1bf248725ebcb3f51780d4b2
Hetul shah <75409654+hetul05@users.noreply.github.com>
2023-03-26 22:08:36 +0530

    range in for loop

---
+++ 40_range in loops.py
@@ -0,0 +1,8 @@
+# for i in range(10):# it will go till (n-1)times 
+#     print(i)
+    
+# for i in range(1,10):# it will start from 1 and ends with (n-1)times 
+#     print(i)
+    
+for i in range(1,10,2):# it will start from 1 and ends with (n-1)times and 2 will replace either number 
+    print(i)
