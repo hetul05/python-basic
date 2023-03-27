e1e1241882724c42efb4fdfb7001f5f30c3ee225
Hetul shah <75409654+hetul05@users.noreply.github.com>
2023-03-27 07:14:51 +0530

    if else in for loop

---
+++ 41_if_else_in_for +loop.py
@@ -0,0 +1,12 @@
+# for i in range(10):
+#     print(i)
+#     if(i==5):
+#         break
+#     else:
+#         print("done")
+
+# continue statment in loop
+for i in range(10):
+    if i==5:
+        continue # here continue is used to skip the print value like here 5 will not print
+    print(i)
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,4 +1,6 @@
-dict={
-#     "panchayat":"parth",
-#     "bigger_panchayat":"yatharth",
-# }
+for i in range(10):
+#     print(i)
+#     if(i==5):
+#         break
+#     else:
+#         print("done")
